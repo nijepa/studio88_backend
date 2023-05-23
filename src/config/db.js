59631f1bca1172413edb2591a00016aadbb4b738
Nikola Pavicevic <nijepa@gmail.com>
2023-05-23 18:45:02 +0200

    packages upgrade

--- src/config/db.js
+++ src/config/db.js
@@ -9,18 +9,18 @@ import 'dotenv/config.js';
 /**
  * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
  * string into the `.env` file
- * 
+ *
  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
- */ 
+ */
 
 const conn = process.env.MONGODB_URI;
 
-const connectDB = () => { 
+const connectDB = () => {
   return mongoose.connect(conn, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false
   });
 };
 // Expose the connection
